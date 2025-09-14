d831d29800a1854c66af20f217865c0f8987f7b6
IImLucasR <thelucasr2@gmail.com>
2025-09-14 20:30:31 -0300

    Fixing CSS modules

--- src/components/Intruduction/index.jsx
+++ src/components/Intruduction/index.jsx
@@ -4,8 +4,8 @@ const Introduction = () => {
     
     return (
         <header>
-            <h1 className={styles.tile}>Calculadora IMC</h1>
-            <h2 className={styles.tile}>Antes de Tudo, Oque é o IMC?</h2>
+            <h1 className={styles.title}>Calculadora IMC</h1>
+            <h2 className={styles.title}>Antes de Tudo, Oque é o IMC?</h2>
             <p className={styles.text}>IMC (Índice de Massa Corporal) é um cálculo usado para avaliar se o peso de uma pessoa está adequado em relação à sua altura. Ele é obtido dividindo o peso (em kg) pela altura ao quadrado (em metros). O resultado indica categorias como abaixo do peso, peso normal, sobrepeso ou obesidade.</p>
         </header>
     )
